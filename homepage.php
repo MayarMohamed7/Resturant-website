1239f6b8d40b8eb184fa2ac696bd40bb363fab68
Marina <93615995+MarinaMaurice@users.noreply.github.com>
2022-05-07 23:23:33 +0200

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -1,6 +1,14 @@
 <html>
+<head>
+  
+<title>Cairo GRND Restaurant</title>
+<link rel="icon" type="image/x-icon" href="Cairo GRND Restaurant.png"> 
+  
+</head>
 <body>
-
+  
+<img src="Cairo GRND Restaurant.png"alt="Cairo GRND Restaurant" width="75" height="75">
+  
 <h1> hi </h1>
 
 </body>
