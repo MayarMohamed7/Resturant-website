8c35cfda96a46ae886f9f48d3aed9bc02be4f424
Marina <93615995+MarinaMaurice@users.noreply.github.com>
2022-05-07 23:31:49 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,19 +1,26 @@
 <html>
 <head>
+	
 <link rel="stylesheet" href="style.css">
+	
 </head>
 <body>
+	
 <div class="lbox">
+	
 <h1> Login </h1> <br>
+	
 <form method = "post" action = "homepage.php">
 <label>Email </label>
- <input type = "text" name = "email"><br>
+<input type = "text" name = "email"><br>
 <label>Password </label>
 <input type = "password" name = "pass"> <br>
 <input type = "submit" value = 'submit'>
 Don't have an account? <a href = "signup.php"> Sign up </a>
 </form>
+	
 </div>
+	
 <?php
 $e = $_POST['email'];
 $p = $_POST['pass'];
@@ -22,26 +29,30 @@ $username = "root";
 $password = "";
 $DB = "project";
 $conn = mysqli_connect($servername, $username, $password, $DB);
-if(!$conn){
-die("connection failed: ".mysqli_connect_error());
-exit();
+	
+if(!$conn)
+{
+   die("connection failed: ".mysqli_connect_error());
+   exit();
 }
+	
 $sql = "SELECT * FROM users WHERE email='$e' AND password='$p'";
+$result = mysqli_query($conn, $sql);
 
-        $result = mysqli_query($conn, $sql);
-
-        if (mysqli_num_rows($result) === 1) {
-
-            $row = mysqli_fetch_assoc($result);
-
-            if ($row['email'] === $e && $row['password'] === $p) {
-                
-                echo "Logged in!";
-			}
-			else{
-				echo "sign up please";
-			}
-		}
+if (mysqli_num_rows($result) === 1)
+{   
+     $row = mysqli_fetch_assoc($result);
+	
+     if ($row['email'] === $e && $row['password'] === $p) 
+     {     
+	     echo "Logged in!";
+     }
+	
+     else
+     {
+       echo "sign up please";
+     }
+}
 ?>
 </body>
 </html>
