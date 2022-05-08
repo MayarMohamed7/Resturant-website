d80ad47e9989ad6962f688e6260e7bf1cb291707
Marina <93615995+MarinaMaurice@users.noreply.github.com>
2022-05-08 16:30:38 +0200

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -1,15 +1,186 @@
-<html>
+<!DOCTYPE html>
+<html lang="en-US">
 <head>
   
 <title>Cairo GRND Restaurant</title>
 <link rel="icon" type="image/x-icon" href="Cairo GRND Restaurant.png"> 
-  
-</head>
-<body>
-  
-<img src="Cairo GRND Restaurant.png"alt="Cairo GRND Restaurant" width="75" height="75">
-  
-<h1> hi </h1>
 
+<style>
+.grid-container
+{
+display:grid;
+grid-template-columns:auto auto auto;
+background-color:Lightgray;
+padding:10px;
+}
+
+.grid-item
+{
+background-color:rgba(255,255,255,0.9);
+border:1px solid rgba(0,0,0,0.2);
+padding:20px;
+text-align:center;
+
+}
+
+body
+{
+font-family:Ariel,Helvetica;
+}
+
+header
+{
+background-color:LightGray;
+padding:30px;
+text-align:center;
+font-size:20px;
+color:white;
+}
+
+nav
+{
+float:left;
+width:30%;
+background:#ccc;
+padding:20px;
+}
+
+nav ul
+{
+list-style-type:square;
+padding:0;
+}
+
+
+a:link,a:visited
+{
+color:white;
+}
+
+a:hover,a:active
+{
+color:blue;
+text-decoration:underline;
+}
+
+
+
+div.container {
+  text-align: center;
+  
+}
+</style>
+
+</head>
+<a href="https://www.google.com/"><img style="padding:10px;" src="Cairo GRND Restaurant.png"alt="Cairo GRND Restaurant" width="110" height="120"> </a>
+<hr>
+
+
+
+<body>
+  <script>
+function BigImage(x)
+{
+x.style.width= "75px";
+x.style.height= "75px";
+
+}
+
+function NormalImage(x)
+{
+x.style.width="60px";
+x.style.height="60px";
+
+
+}
+
+</script>
+<header>
+<h1 id="Top"style="text-align:center; text-decoration:underline;"> Welcome! </h1>  
+</header>
+
+
+
+<br>
+<div class="grid-container">
+<div class="grid-item"> <a href="https://www.google.com/"> <img src="juice.jpg" width="60" height="60" alt="Fresh Juice" onmouseover="BigImage(this)" onmouseout="NormalImage(this)"> </a>
+   <div class="container">
+   <p> soft drinks</p> 
+   </div>
+</div>
+
+
+<div class="grid-item">
+ <img src="juice.jpg" width="60" height="60" alt="Fresh Juice" onmouseover="BigImage(this)" onmouseout="NormalImage(this)">
+  <div class="container">
+  <p> soft drinks</p> 
+  </div>
+</div>
+  
+<div class="grid-item"> <img src="juice.jpg" width="60" height="60" alt="Fresh Juice" onmouseover="BigImage(this)" onmouseout="NormalImage(this)"> 
+   <div class="container">
+   <p> soft drinks</p> 
+   </div>
+</div>
+
+<div class="grid-item"> <img src="juice.jpg" width="60" height="60" alt="Fresh Juice" onmouseover="BigImage(this)" onmouseout="NormalImage(this)"> 
+   <div class="container">
+   <p> soft drinks</p> 
+   </div>
+</div>
+
+<div class="grid-item"> <img src="juice.jpg" width="60" height="60" alt="Fresh Juice" onmouseover="BigImage(this)" onmouseout="NormalImage(this)"> 
+   <div class="container">
+   <p> soft drinks</p> 
+   </div>
+</div>
+
+<div class="grid-item"> <img src="juice.jpg" width="60" height="60" alt="Fresh Juice" onmouseover="BigImage(this)" onmouseout="NormalImage(this)"> 
+   <div class="container">
+   <p> soft drinks</p> 
+   </div>
+</div>
+
+<div class="grid-item"> <img src="juice.jpg" width="60" height="60" alt="Fresh Juice" onmouseover="BigImage(this)" onmouseout="NormalImage(this)"> 
+   <div class="container">
+   <p> soft drinks</p> 
+   </div>
+</div>
+
+<div class="grid-item"> <img src="juice.jpg" width="60" height="60" alt="Fresh Juice" onmouseover="BigImage(this)" onmouseout="NormalImage(this)"> 
+   <div class="container">
+   <p> soft drinks</p> 
+   </div>
+</div>
+
+<div class="grid-item"> <img src="juice.jpg" width="60" height="60" alt="Fresh Juice" onmouseover="BigImage(this)" onmouseout="NormalImage(this)"> 
+   <div class="container">
+   <p> soft drinks</p> 
+   </div>
+</div> 
+ 
+</div>
+<br>
+<a href="#top"> <button type="button"> &uarr; Top</button></a>
+
+
+<footer>
+<br>
+
+
+ <div style="background-color:gray; padding:20px; font-family:sans-serif; ">
+  <p ><a href="mailto:CairoGRNDRestaurant@gmail.com">Inquiries	</p>
+<p></a><a href=""> Careers</a></p>
+
+
+</div>
+</footer>
+
+
+
+<div style="background-color:LightGray; font-family:serif; ">
+<p style="text-align:center; font-Size:15px;"> Copyright &copy; 2022 Cairo GRND Restaurant </p>
 </body>
 </html>
+
+
