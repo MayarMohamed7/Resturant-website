2ecb559724acab5123a34ee97194b549f8dd2be3
Marina <93615995+MarinaMaurice@users.noreply.github.com>
2022-05-10 00:32:38 +0200

    Create Image1.php

---
+++ Image1.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+
+<title>Cairo GRND Restuarant</title>
+<link rel="icon" type="image/x-icon" href="Cairo GRND Restaurant.png">
+<meta charset="UTF-8">
+
+
+<a href="http://localhost/homepage.php"><img title="Cairo GRND Restaurant" style="padding:10px;" src="Cairo GRND Restaurant.png"alt="Cairo GRND Restaurant" width="110" height="120"> </a>
+<br> <br>
+<style>
+body
+{
+font-family:Ariel,Helvetica;
+}
+  
+a:link,a:visited
+{
+color:white;
+}
+
+a:hover
+{
+color:white;
+text-decoration:underline;
+}
+  
+a:active
+{
+color:gray;
+text-decoration:underline;
+}
+
+ul 
+{
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+
+li
+{
+  float: left;
+
+}
+
+li a:link
+{
+  display: block;
+  padding: 12px;
+  color:white;
+}
+  
+li a:hover
+{
+text-decoration:underline;
+background-color:gray;
+}
+
+</style>
+
+<body>
+
+<div style="background-color:black;">
+<ul>
+  <li><a href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li>
+</ul>
+</div>
+<br><br>
+<img src="juice.jpg" width="200px" height="200px" alt="Orange Fresh Juice">
+<br><br>
+
+<a href=""><button type="button" width="100px" height="100px" style="background-color:green color:white"> Order</button> </a>
+
+<form action="">
+  <textarea name="message" rows="10" cols="30" placeholder="Write down your concerns over here"></textarea>
+<br><br>
+<input type="submit">
+<br>
+<footer>
+
+
+
+ <div style="background-color:black; padding:20px; font-family:sans-serif; ">
+  <p ><a href="mailto:CairoGRNDRestaurant@gmail.com">Inquiries	</p>
+<p></a><a href=""> Careers</a></p>
+
+
+</div>
+</footer>
+
+
+
+<div style="background-color:LightGray; font-family:serif; ">
+<p style="text-align:center; font-Size:15px;"> Copyright &copy; 2022 Cairo GRND Restaurant </p>
+</body>
+</form>
+</head>
+
+</html>
