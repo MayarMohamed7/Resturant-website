375a7b341612ab251ff84b37987f7583d8d3bad3
MayarMohamed7 <96928653+MayarMohamed7@users.noreply.github.com>
2022-06-05 20:44:38 +0200

    Update login.php

--- login.php
+++ login.php
@@ -35,31 +35,37 @@ $username = "root";
 $password = "";
 $DB = "project";
 $conn = mysqli_connect($servername, $username, $password, $DB);
-if(isset($_POST['submit']))
-{	
-     $sql = "SELECT * FROM users WHERE email='".$_POST["email"]."' AND password='".$_POST["pass"]."'";
-     $result = mysqli_query($conn, $sql);
+if(isset($_POST['submit'])){
+	if(empty($_POST['pass']) ||
+    empty($_POST['email'])){?>
+   <p class= "error"> <?php echo('Please fill all required fields!');?> </p>
+<?php }
+else{
+		
+$sql = "SELECT * FROM users WHERE email='".$_POST["email"]."' AND password='".$_POST["pass"]."'";
 
-if ($row=mysqli_fetch_array($result))
-{   
-     $_SESSION["email"]=$row["email"];
-     $_SESSION["pass"]=$row["password"];
-     header("Location:homepage.php");
-}
-else if($_POST["email"]=="qualityc@gmail.com" && $_POST["pass"]== "qualityc911"){
+        $result = mysqli_query($conn, $sql);
+
+        if ($row=mysqli_fetch_array($result)) {
+               
+            $_SESSION["email"]=$row["email"];
+		    $_SESSION["pass"]=$row["password"];
+			header("Location:homepage.php");
+		}
+		else if($_POST["email"]=="qualityc@gmail.com" && $_POST["pass"]== "qualityc911"){
 			header("Location:qualityc.php");
-}
-else if($_POST["email"]=="cashier1@gmail.com" && $_POST["pass"]== "cashierv"){
+		}
+		else if($_POST["email"]=="cashier1@gmail.com" && $_POST["pass"]== "cashierv"){
 			header("Location:cashierv.php");
+		}
+			else{
+				?>
+	             <p class= "error"> <?php echo("An email with this password doesn't exist.Sign up please.");?> </p>
+				<?php 
+ 
+			}
+		}
 }
-	
-else
-{
-       ?>
-	   <p class= "error"> <?php echo("An email with this password doesn't exist.Sign up please.");?> </p>
-	<?php 
-}
-}
-?>
+	?>
 </body>
 </html>
