c788e75e4d0d48a23a8defb32d45e4238c56cb2e
HagarElgaabarey <103970966+HagarElgaabarey@users.noreply.github.com>
2022-06-06 02:51:55 +0200

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -172,7 +172,7 @@ x.style.height="250px";
 
  <div style="background-color:black; padding:20px; font-family:sans-serif; ">
   <p> <a title="Inquiries" href="mailto:CairoGRNDRestaurantInquiries@gmail.com">Inquiries</a>	</p>    <!-- mailto:someone@gmail.com is used to send email-->
-  <p> <a title="Careers"   href=""> Careers</a></p>
+  <p> <a title="Careers"   href="careers.php"> Careers</a></p>
 
 
 </div>
