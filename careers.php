becbd544777186f0f781c9fd5687bc821762c087
HagarElgaabarey <103970966+HagarElgaabarey@users.noreply.github.com>
2022-06-08 04:51:27 +0200

    Add files via upload

---
+++ careers.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<link rel="stylesheet"
+href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="../main.css">
+<title>Cairo GRND Restuarant</title>
+<link rel="icon" type="image/x-icon" href="Cairo GRND Restaurant.png">
+
+
+
+<a href="http://localhost/homepage.php "><img title="Cairo GRND Restaurant" style="padding:10px;" src="Cairo GRND Restaurant.png"alt="Cairo GRND Restaurant" width="110" height="120"> </a>
+<br><br>
+
+<style>
+body
+{
+font-family:Ariel,Helvetica;
+}
+a:link,a:visited
+{
+color:white;
+}
+.form {
+  background: #fff;
+  box-shadow: 0 30px 60px 0 rgba(90, 116, 148, 0.4);
+  border-radius: 5px;
+  max-width: 480px;
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  left: 0;
+  right: 0;
+  position: absolute;
+  border-top: 5px solid #0e3721;
+/*   z-index: 1; */
+  animation: bounce 1.5s infinite;
+}
+.formEntry {
+  display: block;
+  margin: 30px auto;
+  min-width: 300px;
+  padding: 10px;
+  border-radius: 2px;
+  border: none;
+  transition: all 0.5s ease 0s;
+}
+.submit {
+  width: 200px;
+  color: white;
+  background-color: #0e3721;
+  font-size: 20px;
+}
+
+.submit:hover {
+  box-shadow: 15px 15px 15px 5px rgba(78, 72, 77, 0.219);
+  transform: translateY(-3px);
+  width: 300px;
+  border-top: 5px solid #0e3750;
+  border-radius: 0%;
+}
+a:hover
+{
+color:white;
+text-decoration:underline;
+}
+a:active
+{
+color:gray;
+text-decoration:underline;
+}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 5px;
+  overflow: hidden;
+}
+
+li {
+  float: left;
+ 
+  
+}
+
+li a:link {
+  display: block;
+  padding: 12px;
+
+  color:white;
+ 
+}
+
+li a:hover
+{
+text-decoration:underline;
+background-color:gray;
+}
+.name {
+  background-color: #ebebeb;
+  color: white;
+}
+.name:hover {
+  border-bottom: 5px solid #0e3721;
+  height: 30px;
+  width: 380px;
+  transition: ease 0.5s;
+}
+
+.email {
+  background-color: #ebebeb;
+  height: 2em;
+}
+
+.email:hover {
+  border-bottom: 5px solid #0e3721;
+  height: 30px;
+  width: 380px;
+  transition: ease 0.5s;
+}
+
+.message {
+  background-color: #ebebeb;
+  overflow: hidden;
+  height: 10rem;
+}
+
+.message:hover {
+  border-bottom: 5px solid #0e3721;
+  height: 12em;
+  width: 380px;
+  transition: ease 0.5s;
+}
+</style>
+</head>
+<body>
+
+<div style="background-color:black;">
+<ul>
+  <li><a href="http://localhost/homepage.php">Home</a></li>
+  <li><a href="http://localhost/News.html">News</a></li>
+  <li><a href="http://localhost/aboutUs.html">About</a></li>
+<li title="Inquiries"><a href="mailto:CairoGRNDRestaurant@gmail.com">Inquiries
+<li><a title="Careers"></a><a href="http://localhost/careers.php"> Careers</a></li>
+</ul>
+</div>
+<br>
+  
+
+<div class="wrapper">
+    
+<h1 style="color: grey"> Join Us </h1> <br>
+    
+<form method = "post" action = "" enctype="multipart/form-data" class=
+"form">
+<input type = "text" class="name formEntry" name = "name" placeholder="Name">
+<input type = "text" class="email formEntry" name = "email" placeholder="Email">
+<input type = "text" class="email formEntry" name = "number" placeholder="Phone Number">
+<label>Please upload your CV down below:</label>
+<input type="file" name="file"><br>
+<button type = "button" class="btn btn-info" class="submit formEntry" onclick="thanks()"> Submit </button>
+<br>
+</form>
+ 
+</div>
+
+
+
+</body>
+</html>
